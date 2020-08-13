aa746a267e38487bef7d880e87cecddd646009c1
Ouokki <ouokki98@gmail.com>
2020-08-13 18:57:35 +0100

    Template random quotes generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/styles.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div id="quote-box" className="container ">
+	<div className="row">
+	    <div className="container">
+          <blockquote className="quote-card yellow-card">
+            <p id="text">
+             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea tempora quia praesentium vitae ratione corporis similique, recusandae incidunt beatae, quae vel magni repellat commodi modi unde suscipit debitis labore odit!
+           </p>
+        
+           <cite id="author" className="ml-1">
+            Author
+          </cite>
+          <div className="row ml-1 mt-1">
+          <button className="btn btn-primary float-left" id="new-quote">New quote</button>
+          <a href="https://twitter.com/intent/tweet" id="tweet-quote" className="ml-4"><button type="button" className="btn btn-social-icon btn-twitter btn-rounded"><i class="fa fa-twitter"></i></button></a>
+          </div>
+          
+          
+        </blockquote>
+
+        </div>
+	</div>
+</div>
   );
 }
 
