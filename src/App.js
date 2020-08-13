56e91fb51a531fbd2eabaf7dd07ee0a4f6213b58
Ouokki <ouokki98@gmail.com>
2020-08-13 23:32:44 +0100

    generate a random quote by an api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React,{ useState } from 'react';
+import axios from 'axios';
 import './styles/styles.css';
 
 
 function App() {
+  const [author, setAuthor] = useState('');
+  const [quote, setQuote] = useState('');
+
+  function generateQuote() {
+    axios.get(`https://random-math-quote-api.herokuapp.com`)
+      .then(res => {
+        const daTa = res.data;
+        setAuthor(daTa.author);
+        setQuote(daTa.quote);
+        
+        
+      })
+  }
   return (
     <div id="quote-box" className="container ">
 	<div className="row">
 	    <div className="container">
           <blockquote className="quote-card yellow-card">
             <p id="text">
-             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea tempora quia praesentium vitae ratione corporis similique, recusandae incidunt beatae, quae vel magni repellat commodi modi unde suscipit debitis labore odit!
+             {quote}
            </p>
         
            <cite id="author" className="ml-1">
-            Author
+            Author : {author}
           </cite>
-          <div className="row ml-1 mt-1">
-          <button className="btn btn-primary float-left" id="new-quote">New quote</button>
-          <a href="https://twitter.com/intent/tweet" id="tweet-quote" className="ml-4"><button type="button" className="btn btn-social-icon btn-twitter btn-rounded"><i class="fa fa-twitter"></i></button></a>
+          <div className="row mt-2">
+          <button onClick={generateQuote} className="btn btn-primary float-left" id="new-quote">New quote</button>
+          <a href="https://twitter.com/intent/tweet" id="tweet-quote" className="ml-4"><button type="button" className="btn btn-social-icon btn-twitter btn-rounded"><i className="fa fa-twitter"></i></button></a>
           </div>
           
           
